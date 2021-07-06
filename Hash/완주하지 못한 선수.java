f4274c5d244173adb515e9dcb4e2a51fff2731c4
PushStone <76048647+pogihae@users.noreply.github.com>
2021-07-06 23:24:04 +0900

    Add Hash/p1

---
+++ Hash/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String p : participant){
+            map.put(p, map.getOrDefault(p,0)+1);
+        }
+        
+        for(String c : completion){
+            map.replace(c,map.get(c)-1);
+        }
+        
+        for(String name : map.keySet()){
+            if(map.get(name) != 0)
+                return name;
+        }
+        
+        return answer;
+    }
+}
