1c3f9ed1e029da845cb04c494c2101dec59c1548
Minseok Park <tir2986@naver.com>
2022-07-08 22:24:54 +0900

    Update MergeTwoLists.kt

--- leetcode/MergeTwoLists.kt
+++ leetcode/MergeTwoLists.kt
@@ -25,3 +25,37 @@ class Solution {
         return l1
     }
 }
+
+//non-recursive
+class Solution2 {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        if (list1 == null) return list2
+        if (list2 == null) return list1
+        
+        val mergedRoot = ListNode(-1)
+        var cur = mergedRoot
+        
+        var p1 = list1
+        var p2 = list2
+        
+        while (p1 != null || p2 != null) {
+            if (p1 == null) {
+                cur.next = p2
+                p2 = p2!!.next
+            } else if (p2 == null) {
+                cur.next = p1
+                p1 = p1!!.next
+            } else if (p1.`val` > p2.`val`) {
+                cur.next = p2
+                p2 = p2.next
+            } else {
+                cur.next = p1
+                p1 = p1.next
+            }
+            
+            cur = cur.next
+        }
+        
+        return mergedRoot.next
+    }
+}
