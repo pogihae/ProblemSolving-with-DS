70b62cdcbdacf942d084ba9f2215488057958c66
Minseok Park <tir2986@naver.com>
2022-07-12 21:21:50 +0900

    Create DailyTemperatures.kt

---
+++ leetcode/stack/DailyTemperatures.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun dailyTemperatures(temperatures: IntArray): IntArray {
+        val stack = Stack<Pair<Int, Int>>()
+        val answer = IntArray(temperatures.size)
+        
+        for ((day, temperature) in temperatures.withIndex()) {
+            while (stack.isNotEmpty() && stack.peek().second < temperature) {
+                val rDay = stack.pop().first
+                answer[rDay] = day - rDay
+            }
+            
+            stack.push(day to temperature)
+        }
+        
+        return answer
+    }
+}
