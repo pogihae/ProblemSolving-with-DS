e6b2ae3bb93ebf1266034172473ae623fc17d9ec
Minseok Park <tir2986@naver.com>
2022-07-25 22:16:42 +0900

    Update numIslands.kt

--- leetcode/graph/numIslands.kt
+++ leetcode/graph/numIslands.kt
@@ -30,3 +30,36 @@ class Solution {
 //kotlin 2d array
 //Array(row) { Array<Any?>(col) {null} }
 //Array(height) { arrayOfNulls<Land?>(width) }
+
+
+class Solution2 {
+    fun numIslands(grid: Array<CharArray>): Int {
+        val visited = Array<BooleanArray>(grid.size) { BooleanArray(grid[0].size) }
+        
+        fun dfs(row: Int, col: Int) {
+            if (row !in 0 until grid.size
+               || col !in 0 until grid[0].size
+               || grid[row][col] != '1'
+               || visited[row][col]) return
+            
+            visited[row][col] = true
+            
+            dfs(row-1, col)
+            dfs(row+1, col)
+            dfs(row, col-1)
+            dfs(row, col+1)
+        }
+        
+        var answer = 0
+        for (r in 0 until grid.size) {
+            for (c in 0 until grid[0].size) {
+                if (grid[r][c] == '1' && visited[r][c] == false){
+                    dfs(r, c)
+                    answer += 1
+                }
+            }
+        }
+        
+        return answer
+    }
+}
