61bc3589e14c6c768e2fd1f0df9584b8001f43f4
Minseok Park <tir2986@naver.com>
2023-02-21 23:01:33 +0900

    Create IsAnagram.java

---
+++ leetcode/sort/IsAnagram.java
@@ -0,0 +1,12 @@
+//string sort
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        return Arrays.compare(sArr, tArr) == 0;
+    }
+}
